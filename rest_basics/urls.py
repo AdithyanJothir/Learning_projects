7f623710638db1d87fe33d881da98ec4b13c3ae1
Jothir Adithyan <adithyanjothir@gmail.com>
2022-06-18 01:28:41 +0530

    learned working class based views

--- rest_basics/urls.py
+++ rest_basics/urls.py
@@ -1,9 +1,10 @@
 from django import views
 from django.contrib import admin
 from django.urls import path
-from .views import Employeeview
+from .views import Employeeview,EmployeeDetailview
 
 urlpatterns = [
     path('employee/', Employeeview.as_view() ,name  = "employee_list"),
+    path('emp_detail/<int:id>',EmployeeDetailview.as_view(),name = "employee_detailview"),
     
 ]
--- rest_basics/views.py
+++ rest_basics/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from requests import delete
 from rest_framework.parsers import JSONParser
 from .models import Employee
 from .serializers import EmployeeSerializer
-from rest_framework.decorators import api_view
 from rest_framework.response import Response 
 from rest_framework import status
 from rest_framework.views import APIView
@@ -27,6 +27,38 @@ class Employeeview(APIView):
             return Response(serializer.errors,status = status.HTTP_400_BAD_REQUEST)
 
 
+    
+class EmployeeDetailview(APIView):
+    
+    def get_object(self,id):
+        try:
+           return Employee.objects.get(id=id)
+            
+
+        except Employee.DoesNotExist:
+            return HttpResponse(status = status.HTTP_404_NOT_FOUND)
+        
+    def get(self,request,id):
+        employee = self.get_object(id)
+        serialized = EmployeeSerializer(employee)
+        return Response(serialized.data)
+    
+    def put(self,request,id):
+        employee = self.get_object(id)
+        serialized = EmployeeSerializer(employee , data=request.data)
+        if serialized.is_valid():
+            serialized.save()
+            return Response(serialized.data, status = status.HTTP_200_OK)
+        else:
+            return Response(status = status.HTTP_400_BAD_REQUEST)
+
+    def delete(self,request,id):
+        employee = self.get_object(id)
+        employee.delete()
+        return Response(status = status.HTTP_204_NO_CONTENT)
+
+        
+        
 
 
 
